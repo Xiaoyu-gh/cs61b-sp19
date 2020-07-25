32b3b06b6eb7f276ebb8a88ebef8d7a57dddda4a
Xiaoyu <cynzxy@gmail.com>
2020-07-25 12:35:43 +0800

    completed HelloNumbers

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		int count = 0;
+		while (x < 45) {
+			x = x + count;
+			System.out.print(x + " ");
+			count = count + 1; 
+		}
+	}
+}
