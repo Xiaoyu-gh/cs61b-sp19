073df1dc642b40d711e41a7080d2a321997771a1
Xiaoyu <cynzxy@gmail.com>
2020-07-30 13:11:44 +0800

    completed proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -62,5 +62,14 @@ public class NBody {
             StdDraw.pause(10);
             currentTime += dt;
         }
+
+        // print out the final state of the universe, i.e. all planets
+        StdOut.printf("%d\n", allBodies.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < allBodies.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                          allBodies[i].xxPos, allBodies[i].yyPos, allBodies[i].xxVel,
+                          allBodies[i].yyVel, allBodies[i].mass, allBodies[i].imgFileName);
+        }
     }
 }
